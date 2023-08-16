c026532e6285dc4121b0d237fe7b9e1a5c981498
JanneL02 <janne_leh@hotmail.com>
2023-08-16 19:49:59 +0300

    1.5: kurssitiedot step5

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,31 +1,31 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={parts}  part2={parts} part3={parts} />
-      <Total part1={parts} part2={parts} part3={parts}/>
+      <Header course={course.name} />
+      <Content part1={course.parts}  part2={course.parts} part3={course.parts} />
+      <Total part1={course.parts} part2={course.parts} part3={course.parts}/>
     </div>
   )
 
- //part1={parts[0].name} 
+
 
 }
 
