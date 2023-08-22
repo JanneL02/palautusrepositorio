4c592b0f2702478cb4f80c7e66c3d333cb4b8dba
JanneL02 <janne_leh@hotmail.com>
2023-08-22 12:16:47 +0300

    1.9 unicafe step4

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
 
-const Statistics = (props) => (
-  <div>{props.text} {props.value} </div>
+const Statistics = (props) =>
+(<div>{props.text} {props.value} </div>)    
 
-)
 
 
 const App = (props) => {
@@ -12,7 +11,6 @@ const App = (props) => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
- 
 
 const handleGoodClick = () =>{
 const newGood = good + 1
@@ -35,6 +33,22 @@ const handleBadClick = () =>{
     setNeutral(newNeutral)
     }
 
+  const CheckIfFeedback = () =>{
+    if(good==0 && bad==0 && neutral==0){
+      return <div>No feedback given</div>
+     }
+     else{
+      return <div><Statistics text={"good"} value={good} />
+      <Statistics text={"neutral"} value={neutral} />
+      <Statistics text={"bad"} value={bad} />
+      <Statistics text={"all"} value={good+neutral+bad} />
+      <Statistics text={"average"} value={(good-bad)/(good+neutral+bad)} />
+      <Statistics text={"positive"} value={(good)/(good+neutral+bad)} />
+     </div>
+     }
+    
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -43,13 +57,12 @@ const handleBadClick = () =>{
       <button onClick={handleBadClick}>bad</button>
       
       <h1>Statistics</h1>
-      <Statistics text={"good"} value={good} />
-      <Statistics text={"neutral"} value={neutral} />
-      <Statistics text={"bad"} value={bad} />
-      <Statistics text={"all"} value={good+neutral+bad} />
-      <Statistics text={"average"} value={(good-bad)/(good+neutral+bad)} />
-      <Statistics text={"positive"} value={(good)/(good+neutral+bad)} />
-     
+
+      <div>
+      <CheckIfFeedback/>
+      </div>
+
+
     </div>
     
   )
